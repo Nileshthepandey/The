259a3eeef4dd4d9409c9a760841225cec9607673
Nileshthepandey <nileshthepandey@gmail.com>
2025-05-20 09:45:18 +0530

    Numbercalculator.py

---
+++ Numbercalculator.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+if b == 0:
+    print("The number can't be divided by zero")
+else:
+    print("Quotient:", a / b)
